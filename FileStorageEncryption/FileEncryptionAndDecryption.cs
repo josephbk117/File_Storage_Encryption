a0b353b54c859a5f66aea1736ab934c6afb96e51
jbk77 <josephbk.117@hotmail.com>
2017-03-14 01:39:13 +0530

    Add project files.

---
+++ FileStorageEncryption/FileEncryptionAndDecryption.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+public static class FileEncryptionAndDecryption
+{    
+    
+    public static void Encrypt(string inputFile,string outputFile,string password)
+    { 
+        FileStream fs = new FileStream(inputFile, FileMode.Open);
+        BinaryReader br = new BinaryReader(fs);
+
+        string formattedName = inputFile.Substring(inputFile.LastIndexOf('\\') + 1);//hello.cs - size : 8
+        byte[] buffer = new byte[(int)fs.Length + formattedName.Length + 1];
+        byte[] temp = br.ReadBytes((int)fs.Length);
+
+        //Put in meta data name of file in first bytes of file name length and first byte is length of name
+        buffer[0] = (byte)formattedName.Length;
+
+        for (int i = 1; i <= formattedName.Length; i++)
+        {
+            buffer[i] = Convert.ToByte(formattedName[i - 1]);
+        }
+        //Buffer is filled with meta data now .. now add the data
+
+        for (int i = formattedName.Length + 1; i < buffer.Length; i++)
+        {
+            int offset = i - (formattedName.Length + 1);
+            buffer[i] = CorrectEncryptByteValue(temp[offset] + 3);
+            buffer[i] = (byte)(buffer[i] ^ password[offset % password.Length]);            
+        }
+        FileStream fr = new FileStream(outputFile, FileMode.Create);
+        BinaryWriter wr = new BinaryWriter(fr);
+
+        wr.Write(buffer);
+
+        wr.Close();
+        fr.Close();
+
+        br.Close();
+        fs.Close();
+    }
+    public static void Decrypt(string inputFile,string password)
+    {
+        FileStream fs = new FileStream(inputFile, FileMode.Open);
+        BinaryReader br = new BinaryReader(fs);
+
+        byte[] buffer = br.ReadBytes((int)fs.Length);
+        int nameSize = buffer[0];
+        string fileName = "";
+
+        for (int i = 1; i <= nameSize; i++)
+        {
+            fileName += Convert.ToChar(buffer[i]);
+        }
+        byte[] data = new byte[buffer.Length - nameSize];
+        int j = 0;
+        for (int i = nameSize + 1; i < buffer.Length; i++)
+        {
+            data[j] = (byte)(buffer[i] ^ password[j % password.Length]);
+            data[j] = CorrectDecryptByteValue(data[j] - 3);
+            j++;
+        }
+
+        FileStream fr = new FileStream(fileName, FileMode.Create);
+        BinaryWriter wr = new BinaryWriter(fr);
+        wr.Write(data);
+
+        wr.Close();
+        fr.Close();
+
+        br.Close();
+        fs.Close();
+    }
+    private static byte CorrectEncryptByteValue(int value)
+    {
+        if (value > 255)
+        {
+            value = (value - 1) % 255;
+        }
+        else if (value < 0)
+        {
+            value = 255 + value;
+        }
+        return (byte)value;
+    }
+    private static byte CorrectDecryptByteValue(int value)
+    {
+        if (value < 0)
+        {
+            value = 255 + (value + 1);
+        }
+        return (byte)value;
+    }
+}
---
+++ FileStorageEncryption/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileStorageEncryption
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string files = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                //Multiselect = true
+            };
+            if (ofd.ShowDialog() == true)
+            {
+                /*foreach (string fileName in ofd.FileNames)
+                {
+                    files += $"{fileName}, ";
+                }*/
+                files = ofd.FileName;
+                openFileTextBox.Text = ofd.FileName;
+            }
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileEncryptionAndDecryption.Encrypt(files, "outputFile.kil",passwordTextBox.Text);
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileEncryptionAndDecryption.Decrypt(files,passwordTextBox.Text);
+        }        
+        
+    }
+
+}
